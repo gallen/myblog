74684bcc28a91267425a522a50de8d7ac8e85736
liulyan <yan.b.liu@oracle.com>
2024-11-24 15:18:35 -0500

    Change social and links

---
+++ content/my_second_article.md
@@ -0,0 +1,13 @@
+Title: My Second article
+Date: 2024-11-23 15:20
+Category: opinion
+
+# My second article
+
+## Winter
+```
+Snowflakes gently fall,
+Whispers of the cold night air,
+Winter's silent grace.
+```
+
--- pelicanconf.py
+++ pelicanconf.py
@@ -8,8 +8,6 @@ TIMEZONE = 'America/New_York'
 
 DEFAULT_LANG = 'en'
 
-THEME = "/Users/liulyan/pelican-themes/Flex"
-
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
@@ -21,14 +19,11 @@ AUTHOR_FEED_RSS = None
 LINKS = (
     ("Pelican", "https://getpelican.com/"),
     ("Python.org", "https://www.python.org/"),
-    ("Jinja2", "https://palletsprojects.com/p/jinja/"),
-    ("You can modify those links in your config file", "#"),
 )
 
 # Social widget
 SOCIAL = (
-    ("You can add links in your config file", "#"),
-    ("Another social link", "#"),
+    ("My class", "https://classroom.google.com/c/NTgwOTA3NjMyNjY2"),
 )
 
 DEFAULT_PAGINATION = 10
